31a9ea6c58d3e8c3a70d2718ed941fe73eb3eb55
blhk0532 <bjorn.leon.henry.kristoffersson@gmail.com>
2025-09-29 15:23:31 +0700

    package.json and gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,6 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'bento.engage-dev.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'github.com',
         port: '',
         pathname: '/**',
